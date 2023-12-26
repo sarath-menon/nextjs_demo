0a5a25e31fc652249c7a38d94ed126421d7a6d46
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-26 22:18:50 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button variant={"purple"} size={"lg"}>
+        Click me
+      </Button>
+    </div>
+  );
+}
