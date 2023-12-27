d2b97c54492ac39a91d619f2bb7dae86c7030ac4
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-27 15:32:19 +0100

    added login

--- app/(marketing)/_componets/navbar.tsx
+++ app/(marketing)/_componets/navbar.tsx
@@ -4,9 +4,12 @@ import { ModeToggle } from "@/components/mode-toggle";
 import { Logo } from "@/app/(marketing)/_componets/logo";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
-
+    const { isAuthenticated, isLoading } = useConvexAuth()
     const scrolled = useScrollTop();
 
     return (
@@ -18,6 +21,27 @@ export const Navbar = () => {
             <Logo />
 
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <p>Loading...</p>
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant={"ghost"} size={"sm"}>
+                                Login
+                            </Button>
+                        </SignInButton>
+
+                        <SignInButton mode="modal">
+                            <Button size={"sm"}>
+                                Get Simplicyti free
+                            </Button>
+                        </SignInButton>
+                    </>
+
+
+
+                )}
                 <ModeToggle />
             </div>
         </div>
