61d418ee0d0e433be17dc6711dc035ad2c0ad25d
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-27 19:35:00 +0100

    making navbar

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button"
+
 const DocumentsPage = () => {
     return (
         <div>
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,10 +1,31 @@
 "use client";
 
+import { ChevronsLeft } from "lucide-react"
+
 export const Navigation = () => {
     return (
-        <div>
-            Navigation
-        </div>
-    );
+        <>
+            {/* Width: width of navbar */}
+            <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
+
+                {/* Show minimize icon when hovering on navbar */}
+                <div role="button" className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition">
+                    <ChevronsLeft className="h-6 w-6 " />
+                </div>
+
+                {/* Menu items */}
+                <div>
+                    <p>Action Items</p>
+                </div>
+
+                <div className="mt-4">
+                    <p>Documents</p>
+                </div>
+
+                <div className="opacity-0 group-hover/sidebar:opacity-100 translate cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+
+            </aside >
+        </>
+    )
 }
 
