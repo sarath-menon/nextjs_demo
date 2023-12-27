4ede1c874c98e74036f309e7973da6175909ab0a
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-27 16:23:04 +0100

    conditioned on loading, signin

--- app/(marketing)/_componets/heading.tsx
+++ app/(marketing)/_componets/heading.tsx
@@ -1,24 +1,46 @@
 "use client"
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 
 export const Heading = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-                Your ideas, Documents and Plans unified. Welcome to <span
-                    className="underline">Simplcyti</span>
+                Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Simplicyti</span>
             </h1>
-
             <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-                Simplicyti is the connected workspace where better , faster work happens
+                Simplicyti is the connected workspace where <br />
+                better, faster work happens.
             </h3>
-
-            <Button className="h-8 s w-30 ml-2">
-                Enter Simplicity <ArrowRight />
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        Enter Simplicyti
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Link>
+                </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Simplicyti free
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            )}
         </div>
-    );
+    )
 }
 
 export default Heading;
