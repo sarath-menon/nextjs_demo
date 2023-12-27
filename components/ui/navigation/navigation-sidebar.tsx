d6c2407c3169c6389422b054986d1e60608a34c6
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-27 09:45:00 +0100

    added heroes

---
+++ components/ui/navigation/navigation-sidebar.tsx
@@ -0,0 +1,9 @@
+import { redirect } from "next/dist/server/api-utils";
+
+export const NavigationSidebar = async () => {
+    return (
+        <div>
+            Navigation Sidebar
+        </div>
+    )
+}
