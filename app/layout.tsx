7761a7751c7e04926c5fb93c461828abe98d7b7e
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-27 15:23:18 +0100

    imported convex

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ConvexClient } from 'convex/browser'
+import { ConvexClientProvider } from '@/components/providers/convex-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,16 +34,20 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-          storageKey='simplicyti-theme'
-        >
 
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+            storageKey='simplicyti-theme'
+          >
+            {children}
+          </ThemeProvider>
+
+        </ConvexClientProvider>
       </body>
     </html>
   )
