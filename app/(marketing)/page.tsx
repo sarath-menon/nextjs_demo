91d3fcce0f499ca0f32830ada8a8ee4fda1a0b7b
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-27 10:17:17 +0100

    added footer

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,16 +1,17 @@
 import { Button } from "@/components/ui/button";
 import Heading from "./_componets/heading";
 import Heroes from "./_componets/heroes";
+import Footer from "./_componets/footer";
 
 const MarketingPage = () => {
   return (
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
-
         <Heroes />
-
       </div>
+
+      <Footer />
     </div>
   );
 }
