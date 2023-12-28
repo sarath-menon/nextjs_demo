2e85f454afa204ea66cc18aa9801ae8f248b5221
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-28 12:03:55 +0100

    code cleanup

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -7,37 +7,54 @@ import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <section>
-      <div className="overflow-hidden flex flex-col items-center justify-center text-center mt-10 px:4 gap-10 max-w-5xl">
 
-        <TitleSection pill="Your workspace perfected" title="prod platform" />
+    <>
+      {/* Hero */}
+      <section>
+        <div className="overflow-hidden flex flex-col items-center justify-center text-center mt-10 px:4 gap-10 max-w-5xl">
 
-        <div>
-          <Button variant="secondary"> Click me</Button>
-        </div>
+          <TitleSection pill="Your workspace perfected" title="prod platform" />
 
-        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
-          <Image
-            src="/reading.png"
-            fill
-            className="object-contain dark:hidden"
-            alt="Reading"
-          />
-          <Image
-            src="/reading-dark.png"
-            fill
-            className="object-contain hidden dark:block"
-            alt="Reading"
-          />
+          <div>
+            <Button variant="secondary"> Click me</Button>
+          </div>
 
-        </div>
+          {/* Hero Images */}
+          <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+            <Image
+              src="/reading.png"
+              fill
+              className="object-contain dark:hidden"
+              alt="Reading"
+            />
+            <Image
+              src="/reading-dark.png"
+              fill
+              className="object-contain hidden dark:block"
+              alt="Reading"
+            />
 
-        {/* <Heading /> */}
-        {/* <Heroes /> */}
-      </div >
+          </div>
 
-      <Footer />
-    </section >
+          {/* Testimonials */}
+
+          {/* <Heading /> */}
+          {/* <Heroes /> */}
+        </div >
+      </section >
+
+      {/* Testimonials */}
+      <section className="relative">
+
+      </section>
+
+
+      {/* Footer  */}
+      <section>
+        <Footer />
+      </section>
+
+    </>
   );
 }
 
