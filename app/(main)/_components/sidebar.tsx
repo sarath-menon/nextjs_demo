f091e87a158267b7348b96ecd5c84b806f91f9aa
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-28 23:53:24 +0100

    added github user item

--- app/(main)/_components/sidebar.tsx
+++ app/(main)/_components/sidebar.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Playlist } from "../data/playlists"
+import { UserItem } from "./notion_clone/user-item"
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
     playlists: Playlist[]
@@ -13,9 +14,17 @@ export function Sidebar({ className, playlists }: SidebarProps) {
         <div className={cn("pb-12", className)}>
             <div className="space-y-4 py-4">
                 <div className="px-3 py-2">
+
+                    {/* Github user tag */}
+                    <div className="py-4">
+                        <UserItem />
+                    </div>
+
+
                     <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
                         Discover
                     </h2>
+
                     <div className="space-y-1">
                         <Button variant="secondary" className="w-full justify-start">
                             <svg
@@ -72,6 +81,7 @@ export function Sidebar({ className, playlists }: SidebarProps) {
                         </Button>
                     </div>
                 </div>
+
                 <div className="px-3 py-2">
                     <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
                         Library
