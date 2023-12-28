a8e346b70e8545657387dae87fca5e53d6e36fc1
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-28 17:50:27 +0100

    added toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClient } from 'convex/browser'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -44,6 +45,8 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey='simplicyti-theme'
           >
+
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
 
