3d4cd3fb6fe2bf6d180f65f2a99df5ee124855cb
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-28 12:26:46 +0100

    added testimonial icons

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,6 +4,8 @@ import Heroes from "./_componets/heroes";
 import Footer from "./_componets/footer";
 import TitleSection from "./_componets/title";
 import Image from "next/image";
+import { Divide } from "lucide-react";
+import { CLIENTS } from "@/lib/constants";
 
 const HomePage = () => {
   return (
@@ -45,14 +47,29 @@ const HomePage = () => {
 
       {/* Testimonials */}
       <section className="relative">
+        <div className="overflow-hidden flex absolute  
+        after:content[''] after:dark:from-brand/dark after:to-transparent after:from-background after-bg-gradient-to-l after:right-0 after:top-0 after:bottom-0 after:w-20 after:z-10 after:absolute
+        before:content[''] before:dark:from-brand/dark before:to-transparent before:from-background before-bg-gradient-to-r before:left-0 before:bottom-0 before:top-0 before:w-20 before:z-10 before:absolute"
+        >
 
-      </section>
+          {[...Array(2)].map((arr) => <div
+            key={arr} className="flex flex-nowrap animate-slide"> {CLIENTS.map((client) => (<div
+              key={client.alt} className="relative w-[200px] m-20 shrink-0 flex items-center">
+
+              <Image src={client.logo} alt={client.alt} width={200} height={100} className="object-contain max-w-none" >
+
+              </Image>
+            </div>))}
+          </div>)}
+
+        </div>
+      </section >
 
 
       {/* Footer  */}
-      <section>
+      {/* < section >
         <Footer />
-      </section>
+      </section > */}
 
     </>
   );
