af8e53e1d3df0e33af79b399d034434539ff922b
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-28 11:57:46 +0100

    addded image

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,30 +3,41 @@ import Heading from "./_componets/heading";
 import Heroes from "./_componets/heroes";
 import Footer from "./_componets/footer";
 import TitleSection from "./_componets/title";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
     <section>
-      <div className="flex 
-      flex-col 
-      items-center 
-      justify-center 
-      text-center 
-      mt-10
-      px:4
-      sm:px-6 
-      sm:flex
-      sm:flex-col
-      gap-10">
+      <div className="overflow-hidden flex flex-col items-center justify-center text-center mt-10 px:4 gap-10 max-w-5xl">
 
         <TitleSection pill="Your workspace perfected" title="prod platform" />
 
+        <div>
+          <Button variant="secondary"> Click me</Button>
+        </div>
+
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+          <Image
+            src="/reading.png"
+            fill
+            className="object-contain dark:hidden"
+            alt="Reading"
+          />
+          <Image
+            src="/reading-dark.png"
+            fill
+            className="object-contain hidden dark:block"
+            alt="Reading"
+          />
+
+        </div>
+
         {/* <Heading /> */}
         {/* <Heroes /> */}
-      </div>
+      </div >
 
-      {/* <Footer /> */}
-    </section>
+      <Footer />
+    </section >
   );
 }
 
