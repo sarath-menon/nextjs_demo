f7bf08407c6e3cb7f42fb83d2772925d814dd425
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-12-31 10:34:50 +0100

    added svg to buttons

--- app/(main)/_components/sidebar.tsx
+++ app/(main)/_components/sidebar.tsx
@@ -38,46 +38,35 @@ export function Sidebar({ className, items, ...props }: SidebarProps) {
                         Discover
                     </h2>
 
-                    <div className="space-y-1">
-                        <Button variant="secondary" className="w-full justify-start">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="mr-2 h-4 w-4"
-                            >
-                                <circle cx="12" cy="12" r="10" />
-                                <polygon points="10 8 16 12 10 16 10 8" />
-                            </svg>
-                            Listen Now
-                        </Button>
-                    </div>
-
                     <nav
                         className={cn(
-                            "flex space-x-2 flex-col  space-y-1",
+                            "flex flex-col  space-y-1",
                             className
                         )}
                         {...props}
                     >
                         {items.map((item) => (
-                            <Link
-                                key={item.href}
-                                href={item.href}
-                                className={cn(
-                                    buttonVariants({ variant: "ghost" }),
-                                    pathname === item.href
-                                        ? "bg-muted hover:bg-muted"
-                                        : "hover:bg-transparent hover:underline",
-                                    "justify-start"
-                                )}
-                            >
-                                {item.title}
-                            </Link>
+                            <div className="space-y-1">
+
+                                <Button variant="secondary" className="w-full justify-start">
+
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        className="mr-2 h-4 w-4"
+                                    >
+                                        <circle cx="12" cy="12" r="10" />
+                                        <polygon points="10 8 16 12 10 16 10 8" />
+                                    </svg>
+
+                                    {item.title}
+                                </Button>
+                            </div>
                         ))}
                     </nav>
 
