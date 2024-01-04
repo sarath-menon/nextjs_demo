ea2872019b1c051bfd5499de365a245bbf088174
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-01-04 04:59:13 +0100

    added dashboard cards

--- app/(main)/(routes)/forms/home/page.tsx
+++ app/(main)/(routes)/forms/home/page.tsx
@@ -10,6 +10,7 @@ import { toast } from "sonner";
 import { Separator } from "@radix-ui/react-dropdown-menu";
 import { Requirements } from "./form"
 import React from "react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 
 const DocumentsPage = () => {
@@ -43,6 +44,121 @@ const DocumentsPage = () => {
                 <p className="text-muted">
                     Manage your account settings and set e-mail preferences.
                 </p>
+            </div>
+
+            {/* Cards row */}
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                {/* Publications */}
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                            Publications
+                        </CardTitle>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            className="h-4 w-4 text-muted-foreground"
+                        >
+                            <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                        </svg>
+
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">12</div>
+                        <p className="text-xs text-muted-foreground">
+                            4 last year
+                        </p>
+                    </CardContent>
+                </Card>
+
+                {/* {Active projects} */}
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                            Active projects
+                        </CardTitle>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            className="h-4 w-4 text-muted-foreground"
+                        >
+                            <rect width="20" height="14" x="2" y="5" rx="2" />
+                            <path d="M2 10h20" />
+                        </svg>
+
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">5</div>
+                        <p className="text-xs text-muted-foreground">
+                            12 total
+                        </p>
+                    </CardContent>
+                </Card>
+
+                {/* Current members */}
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">Current members</CardTitle>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            className="h-4 w-4 text-muted-foreground"
+                        >
+                            <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                            <circle cx="9" cy="7" r="4" />
+                            <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                        </svg>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">7</div>
+                        <p className="text-xs text-muted-foreground">
+                            18 since the start
+                        </p>
+                    </CardContent>
+                </Card>
+
+                {/* Money spent */}
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                            Money spent
+                        </CardTitle>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            className="h-4 w-4 text-muted-foreground"
+                        >
+                            <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                        </svg>
+
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">$ 10,500</div>
+                        <p className="text-xs text-muted-foreground">
+                            $890 last month
+                        </p>
+                    </CardContent>
+                </Card>
             </div>
 
             <Requirements />
