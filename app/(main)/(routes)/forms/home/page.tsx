c46618572071e9df91414200f07acba1467211e6
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-01-04 05:21:25 +0100

    component for dashboard cards

--- app/(main)/(routes)/forms/home/page.tsx
+++ app/(main)/(routes)/forms/home/page.tsx
@@ -11,6 +11,7 @@ import { Separator } from "@radix-ui/react-dropdown-menu";
 import { Requirements } from "./form"
 import React from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { DashboardCard } from "@/app/(main)/_components/dashboard-card";
 
 
 const DocumentsPage = () => {
@@ -46,124 +47,45 @@ const DocumentsPage = () => {
                 </p>
             </div>
 
-            {/* Cards row */}
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                {/* Publications */}
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                        <CardTitle className="text-sm font-medium">
-                            Publications
-                        </CardTitle>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="h-4 w-4 text-muted-foreground"
-                        >
-                            <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                        </svg>
+            {/* Dashboard Cards row */}
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 my-6">
 
-                    </CardHeader>
-                    <CardContent>
-                        <div className="text-2xl font-bold">12</div>
-                        <p className="text-xs text-muted-foreground">
-                            4 last year
-                        </p>
-                    </CardContent>
-                </Card>
+                {/* Publications */}
+                <DashboardCard
+                    path="/icons/symbol-file.svg"
+                    title="Publications"
+                    metric="12"
+                    subtitle="4 last year">
+                </DashboardCard>
 
                 {/* {Active projects} */}
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                        <CardTitle className="text-sm font-medium">
-                            Active projects
-                        </CardTitle>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="h-4 w-4 text-muted-foreground"
-                        >
-                            <rect width="20" height="14" x="2" y="5" rx="2" />
-                            <path d="M2 10h20" />
-                        </svg>
-
-                    </CardHeader>
-                    <CardContent>
-                        <div className="text-2xl font-bold">5</div>
-                        <p className="text-xs text-muted-foreground">
-                            12 total
-                        </p>
-                    </CardContent>
-                </Card>
+                <DashboardCard
+                    path="/icons/symbol-file.svg"
+                    title="Active projects"
+                    metric="5"
+                    subtitle="12 total">
+                </DashboardCard>
 
                 {/* Current members */}
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                        <CardTitle className="text-sm font-medium">Current members</CardTitle>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="h-4 w-4 text-muted-foreground"
-                        >
-                            <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                            <circle cx="9" cy="7" r="4" />
-                            <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                        </svg>
-                    </CardHeader>
-                    <CardContent>
-                        <div className="text-2xl font-bold">7</div>
-                        <p className="text-xs text-muted-foreground">
-                            18 since the start
-                        </p>
-                    </CardContent>
-                </Card>
+                <DashboardCard
+                    path="/icons/symbol-file.svg"
+                    title="Current members"
+                    metric="7"
+                    subtitle="18 since the start">
+                </DashboardCard>
 
                 {/* Money spent */}
-                <Card>
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                        <CardTitle className="text-sm font-medium">
-                            Money spent
-                        </CardTitle>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="h-4 w-4 text-muted-foreground"
-                        >
-                            <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                        </svg>
-
-                    </CardHeader>
-                    <CardContent>
-                        <div className="text-2xl font-bold">$ 10,500</div>
-                        <p className="text-xs text-muted-foreground">
-                            $890 last month
-                        </p>
-                    </CardContent>
-                </Card>
+                <DashboardCard
+                    path="/icons/symbol-file.svg"
+                    title=" Money spent"
+                    metric="$ 10,500"
+                    subtitle=" $890 last month">
+                </DashboardCard>
             </div>
 
             <Requirements />
 
-        </div>
+        </div >
 
     );
 }
---
+++ app/(main)/_components/dashboard-card.tsx
@@ -0,0 +1,30 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+
+export function DashboardCard(props: any) {
+    return (
+        <>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                        {props.title}
+                    </CardTitle>
+
+                    <div className="brightness-50">
+                        <Image src={props.path} height={20} width={20} alt="" />
+                    </div>
+
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">
+                        {props.metric}
+                    </div>
+                    <p className="text-xs brightness-50">
+                        {props.subtitle}
+                    </p>
+                </CardContent>
+            </Card>
+        </>
+    );
+}
+
