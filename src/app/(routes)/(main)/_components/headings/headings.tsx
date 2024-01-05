3034b3a5a77de00b15fa5b3af6b4d4e1dfa5fdb7
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-01-05 21:50:08 +0100

    added subheading component

---
+++ src/app/(routes)/(main)/_components/headings/headings.tsx
@@ -0,0 +1,36 @@
+
+
+export function TopHeading(props: any) {
+    return (
+
+        < div className="" >
+            <h2 className="text-5xl my-4">
+                {props.heading}
+            </h2>
+
+            <p className="brightness-50 text-lg">
+                {props.text}
+            </p>
+        </div >
+
+    )
+
+};
+
+export function SubHeading(props: any) {
+    return (
+
+        <div>
+            <h2 className="text-3xl tracking-tight">
+                {props.subheading}
+            </h2>
+
+            <p className="brightness-50 text-base">
+                {props.text}
+            </p>
+        </div>
+
+
+    )
+};
+
