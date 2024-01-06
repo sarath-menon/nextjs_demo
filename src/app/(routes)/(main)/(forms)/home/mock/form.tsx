fbb6036a243737e8dbf14da42e34901fb02d4c0a
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-01-06 12:06:54 +0100

    added table

--- src/app/(routes)/(main)/(forms)/home/mock/form.tsx
+++ src/app/(routes)/(main)/(forms)/home/mock/form.tsx
@@ -7,24 +7,6 @@ import * as z from "zod"
 import { Button } from "@/src/components/ui/button"
 import { CreateObjectiveDialog } from "@/src/app/(routes)/(main)/_components/dialogs/objective_creator"
 
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/src/components/ui/form"
-
-import { Input } from "@/src/components/ui/input"
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/src/components/ui/select"
 
 import { Textarea } from "@/src/components/ui/textarea"
 import { toast } from "@/src/components/ui/use-toast"
@@ -34,63 +16,12 @@ import { CreateConstraintDialog } from "@/src/app/(routes)/(main)/_components/di
 import { OBJECTIVES } from "@/src/app/(routes)/(main)/data/objectives"
 import { objectiveColumns } from "@/src/app/(routes)/(main)/(forms)/home/table-columns"
 import { SubHeading } from "../../../_components/headings/page-style"
+import { DataTableV2 } from "../../../_components/datatable_v2/data-table"
+import { SUB_PROJECTS } from "../../../data/subprojects"
+import { subprojectColumns } from "../../subprojects/table-columns"
 
-const profileFormSchema = z.object({
-    type: z
-        .string({
-            required_error: "Please select a requirement type.",
-        })
-        .email(),
-
-    description: z.string().max(160).min(10),
-
-    rationale: z.string().max(160).min(10),
-
-    test_procedure: z.string().max(160).min(10),
-
-    urls: z
-        .array(
-            z.object({
-                value: z.string().url({ message: "Please enter a valid URL." }),
-            })
-        )
-        .optional(),
-})
-
-type RequirementsValues = z.infer<typeof profileFormSchema>
-
-// This can come from your database or API.
-const defaultValues: Partial<RequirementsValues> = {
-    description: "Chopsticks as propellers",
-    urls: [
-        { value: "https://shadcn.com" },
-        { value: "http://twitter.com/shadcn" },
-    ],
-}
 
 export function Requirements() {
-
-    const form = useForm<RequirementsValues>({
-        resolver: zodResolver(profileFormSchema),
-        defaultValues,
-        mode: "onChange",
-    })
-
-    const { fields, append } = useFieldArray({
-        name: "urls",
-        control: form.control,
-    })
-
-    function onSubmit(data: RequirementsValues) {
-        toast({
-            title: "You submitted the following values:",
-            description: (
-                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-                    <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-                </pre>
-            ),
-        })
-    }
 
     return (
 
@@ -106,9 +37,13 @@ export function Requirements() {
                     subheading="Objectives"
                     text="" />
 
+                <div className="flex">
+                    <DataTableV2 data={SUB_PROJECTS} columns={subprojectColumns} />
+                </div>
+
+
                 <div className="py-6">
                     {/* <DataTableV2 data={OBJECTIVES} columns={objectiveColumns} /> */}
-
                     <CreateObjectiveDialog />
                 </div>
             </div>
