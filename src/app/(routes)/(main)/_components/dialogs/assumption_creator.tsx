98d457c51593bc9c10cef72af75505ca35554931
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-01-06 11:16:55 +0100

    use dialog skeletons

--- src/app/(routes)/(main)/_components/dialogs/assumption_creator.tsx
+++ src/app/(routes)/(main)/_components/dialogs/assumption_creator.tsx
@@ -1,102 +1,60 @@
-import { Button } from "@/src/components/ui/button"
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/src/components/ui/dialog"
-
 import { Input } from "@/src/components/ui/input"
 import { Label } from "@/src/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/src/components/ui/select"
+import { TiptapEntry } from "../text-editors/tiptap/tiptap-editor"
+import { DialogElement, DialogSkeleton } from "./dialog-skeleton"
+
+function customSection() {
+    return (
+        <>
+            <div className="grid grid-cols-2 gap-4">
+
+                {/* Requirement nature */}
+                <div className="grid gap-2">
+                    <Label htmlFor="area" className="text-lg">
+                        Nature
+                    </Label>
+
+                    <Select defaultValue="billing">
+                        <SelectTrigger id="area">
+                            <SelectValue placeholder="Select" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="team">Scientific question</SelectItem>
+                            <SelectItem value="billing">Engineering goal</SelectItem>
+                            <SelectItem value="account">Mixed</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+            </div>
+
+            <DialogElement
+                title="Description"
+                custom_section={TiptapEntry}
+            />
+
+            <DialogElement
+                title="Rationale"
+                custom_section={TiptapEntry}
+            />
+
+            <DialogElement
+                title="How to test"
+                custom_section={TiptapEntry}
+            />
+        </>
+    )
+}
+
 
 export function CreateAssumptionDialog() {
     return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <Button variant="secondary">
-                    Add new
-                </Button>
-            </DialogTrigger>
 
-            <DialogContent className="sm:max-w-[475px]">
-
-                <DialogHeader>
-                    <DialogTitle>
-                        Add new assumption
-                    </DialogTitle>
-
-                    <DialogDescription>
-                        This will save the current playground state as a preset which you
-                        can access later or share with others.
-                    </DialogDescription>
-
-                </DialogHeader>
-
-                <div className="grid grid-cols-2 gap-4">
-
-                    {/* Requirement nature */}
-                    {/* <div className="grid gap-2">
-                        <Label htmlFor="area">Nature</Label>
-
-                        <Select defaultValue="billing">
-                            <SelectTrigger id="area">
-                                <SelectValue placeholder="Select" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="team">Scientific question</SelectItem>
-                                <SelectItem value="billing">Engineering goal</SelectItem>
-                                <SelectItem value="account">Mixed</SelectItem>
-                            </SelectContent>
-                        </Select>
-                    </div> */}
-
-
-                    {/* Requirement type */}
-                    {/* <div className="grid gap-2">
-                        <Label htmlFor="area">Area</Label>
-                        <Select defaultValue="billing">
-                            <SelectTrigger id="area">
-                                <SelectValue placeholder="Type" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="team">Functional</SelectItem>
-                                <SelectItem value="billing">Financial</SelectItem>
-                                <SelectItem value="account">Regulatory</SelectItem>
-                                <SelectItem value="deployments">Performance</SelectItem>
-                                <SelectItem value="support">Environment</SelectItem>
-                            </SelectContent>
-                        </Select>
-                    </div> */}
-                </div>
-
-                <div className="grid gap-4 py-4">
-
-                    <div className="grid gap-2">
-                        <Label htmlFor="description">Description</Label>
-                        <Input id="description" />
-                    </div>
-
-                    <div className="grid gap-2">
-                        <Label htmlFor="name">Rationale</Label>
-                        <Input id="name" autoFocus />
-                    </div>
-
-                    {/* <div className="grid gap-2">
-                        <Label htmlFor="name">How to test</Label>
-                        <Input id="name" autoFocus />
-                    </div> */}
-
-                </div>
-
-                <DialogFooter>
-                    <Button type="submit">Save</Button>
-                </DialogFooter>
-
-            </DialogContent>
-        </Dialog >
+        <DialogSkeleton
+            button_text="Add new"
+            title="Add new objective"
+            description="This will save the current playground state as a preset which you can access later or share with others."
+            custom_section={customSection}
+        />
     )
 }
