f7d2faecb69bd0f45559309aff9c9845cf21760b
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-01-07 21:07:34 +0100

    sidebar apper/disapper works

--- src/components/navigation/sidebar/icon-sidebar.tsx
+++ src/components/navigation/sidebar/icon-sidebar.tsx
@@ -5,18 +5,20 @@ import { ScrollArea } from "@/src/components/ui/scroll-area"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import Image from "next/image"
-import { useRef } from "react"
+import { createContext, useRef, useState } from "react"
 
 import { useCycle } from "framer-motion"
 import { UserItem } from "./notion_clone/user-item"
 import { DocumentList } from "./notion_clone/document-list"
-import { Plus } from "lucide-react"
+import { ChevronFirst, ChevronLast, Plus } from "lucide-react"
 import {
     Tooltip,
     TooltipContent,
     TooltipProvider,
     TooltipTrigger,
 } from "@/src/components/ui/tooltip"
+import { Sidebar } from "./notion_clone/sidebar"
+import { SIDEBAR_ITEMS } from "@/src/lib/constants"
 
 
 interface IconSidebarProps extends React.HTMLAttributes<HTMLElement> {
@@ -27,18 +29,23 @@ interface IconSidebarProps extends React.HTMLAttributes<HTMLElement> {
     }[]
 }
 
+function log_msg() {
+    console.log("selva")
+}
+
+
 export function IconSidebar({ className, items, ...props }: IconSidebarProps) {
 
     // variables  
     const pathname = usePathname()
     const containerRef = useRef(null);
-    const [isOpen, toggleOpen] = useCycle(false, true)
+
+    const [isExpanded, setExpanded] = useState(true)
 
 
     return (
 
         <div className=" py-6">
-
             <nav
                 className={cn(
                     "flex flex-col space-y-3",
@@ -51,14 +58,17 @@ export function IconSidebar({ className, items, ...props }: IconSidebarProps) {
                         <TooltipProvider>
                             <Tooltip delayDuration={300}  >
                                 <TooltipTrigger asChild>
-                                    <button >
-                                        <div className="flex mx-3 my-1 items-center justify-center brightness-100 hover:brightness-200">
+                                    <button onClick={() => setExpanded(!isExpanded)} >
 
+                                        <div className="flex mx-3 my-1 items-center justify-center brightness-100 hover:brightness-200">
                                             <Link href={item.href}>
                                                 <Image src={item.svg_path} height="25" width="25" alt="" />
                                             </Link>
-
                                         </div>
+
+                                        {/* SIdebar */}
+                                        {/* {expanded ? <ChevronFirst /> : <ChevronLast    />} */}
+
                                     </button>
 
                                 </TooltipTrigger>
@@ -71,8 +81,22 @@ export function IconSidebar({ className, items, ...props }: IconSidebarProps) {
 
                     </div>
                 ))}
+
+                {/* Shad-cn text sidebar  */}
+
+                <span> {isExpanded ?
+                    <div className="border-r flex pr-1 w-[240px]">
+                        <Sidebar items={SIDEBAR_ITEMS} />
+                    </div>
+
+                    : ""} </span>
+
+
+
             </nav>
         </div>
     )
 }
+
+
 
