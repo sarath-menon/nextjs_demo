f369423b8c2a4197602860ad999ff4467d5948fe
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-01-07 16:06:31 +0100

    better looks

--- src/app/(routes)/(landing)/_componets/title.tsx
+++ src/app/(routes)/(landing)/_componets/title.tsx
@@ -11,9 +11,9 @@ const TitleSection: React.FC<TitleSectionProps> = ({ title, subheading, pill }) 
     return (
 
         <React.Fragment>
-            <section className="flex flex-col gap-4 justify-center md:items-center my-8">
-                <article className="rounded-full p-[1px] text-sm dark:bg-gradient-to-r dark:from-brand/primaryPurple dark:to-brand/primaryBlue">
-                    <div className="rounded-full px-3 py-1 dark:bg-black">
+            <section className="flex flex-col gap-4 justify-center md:items-center mb-6">
+                <article className="rounded-full p-[1px] text-base dark:bg-gradient-to-r dark:from-brand/primaryPurple dark:to-brand/primaryBlue">
+                    <div className="rounded-full px-4 py-2 dark:bg-black">
                         {pill}
                     </div>
                 </article>
@@ -27,7 +27,7 @@ const TitleSection: React.FC<TitleSectionProps> = ({ title, subheading, pill }) 
                         <p className="dark:text-washed-purple/washed-purple-700 sm:max-w-[450px] md-text-center"></p>
 
                     </>) : (
-                    <h1 className="text-left text-4xl sm:text-6xl sm:max-w-[850px] md:text:center font-semibold mt-4">
+                    <h1 className="text-left text-4xl sm:text-6xl sm:max-w-[850px] md:text:center font-semibold mt-6">
                         {title}
                     </h1>
                 )}
